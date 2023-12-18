08bd4d075f54d357a581f0c958e8a6ba7cc7180c
Arwamohammedyehya <154276971+Arwamohammedyehya@users.noreply.github.com>
2023-12-19 00:09:46 +0200

    Add files via upload

---
+++ A.cpp
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome TO Our Hotel</title>ا</title>
+    <style>
+        body {
+            background-image: url("1.jpg"); 
+            background-size: cover ;
+            background-position: center;
+            background-attachment: fixed;
+            height: 90vh;
+            margin: 0;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            color: #ffffff;
+            font-family: Arial, sans-serif;
+        }
+
+        #welcome-container {
+            text-align: center;
+        }
+
+        #enter-button {
+            padding: 10px 20px;
+            font-size: 16px;
+            background-color: #3498db;
+            color: #000000;
+            border: none;
+            cursor: pointer;
+        }
+
+        #hotel-image {
+            width: auto; 
+            height: auto;
+            margin-top: auto;
+        }
+    </style>
+</head>
+<body>
